b9166e223e2f64df755c16c4315aa9a3a1a6b937
saramohammadi <saramohamadikahvarin@gmail.com>
2024-08-16 17:54:27 +0330

    Define models for User,Benefactor, Charity, and Task

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,17 @@
 from django.contrib.auth.models import AbstractUser
-
+from django.db import models
 
 class User(AbstractUser):
-    pass
+    address = models.TextField(null=True, blank=True)
+    age = models.PositiveSmallIntegerField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    gender = models.CharField(
+        max_length=1, 
+        choices=[('M', 'Male'), ('F', 'Female')], 
+        null=True, 
+        blank=True
+    )
+    phone = models.CharField(max_length=15, null=True, blank=True)
+
+    def __str__(self):
+        return self.username
--- charities/models.py
+++ charities/models.py
@@ -1,13 +1,74 @@
 from django.db import models
-
+from django.conf import settings
 
 class Benefactor(models.Model):
-    pass
+    EXPERIENCE_CHOICES = [
+        (0, 'Beginner'),
+        (1, 'Intermediate'),
+        (2, 'Expert'),
+    ]
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE
+    )
+    experience = models.SmallIntegerField(
+        choices=EXPERIENCE_CHOICES, 
+        default=0
+    )
+    free_time_per_week = models.PositiveSmallIntegerField(default=0)
+
+    def __str__(self):
+        return self.user.username
 
 
 class Charity(models.Model):
-    pass
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=50)
+    reg_number = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
 
 
 class Task(models.Model):
-    pass
+    STATUS_CHOICES = [
+        ('P', 'Pending'),
+        ('W', 'Waiting'),
+        ('A', 'Assigned'),
+        ('D', 'Done'),
+    ]
+    
+    id = models.AutoField(primary_key=True)
+    assigned_benefactor = models.ForeignKey(
+        'Benefactor', 
+        on_delete=models.SET_NULL, 
+        null=True, 
+        blank=True
+    )
+    charity = models.ForeignKey(
+        'Charity', 
+        on_delete=models.CASCADE
+    )
+    age_limit_from = models.IntegerField(null=True, blank=True)
+    age_limit_to = models.IntegerField(null=True, blank=True)
+    date = models.DateField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    gender_limit = models.CharField(
+        max_length=1, 
+        choices=[('M', 'Male'), ('F', 'Female')],
+        null=True, 
+        blank=True
+    )
+    state = models.CharField(
+        max_length=1, 
+        choices=STATUS_CHOICES, 
+        default='P'
+    )
+    title = models.CharField(max_length=60)
+    def __str__(self):
+        return self.title
