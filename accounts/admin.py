a4e9580ac769a81ec4fa0e697c4d40a2d981fde7
saramohammadi <saramohamadikahvarin@gmail.com>
2024-08-18 00:19:20 +0330

    Add custom Admin interface for User model and update migrations

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    list_editable = ('is_staff', 'is_active')
+
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'password'),
+        }),
+        ('Personal info', {
+            'fields': ('first_name', 'last_name', 'email', 'gender', 'age', 'description'),
+        }),
+        ('Contact info', {
+            'fields': ('phone', 'address'),
+        }),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {
+            'fields': ('last_login', 'date_joined'),
+        }),
+    )  
